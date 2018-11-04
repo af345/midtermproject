fd2c98fa69e54dbd9dcd8709a0404c92255d0a60
alexandraflores <af345@njit.edu>
2018-11-04 17:56:24 -0500

    7.  Create a controller, route, and view to show a contact form and add it to the main menu of your site.
    8.  Create a controller, route, and view to show an about page and add it to the main menu of your site.

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@home') ->name('home');
+Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/contact', 'PagesController@contact')->name('contact');
+Route::post('/contact', 'PagesController@store')->name('contact.store');
+Route::get('/thanks/{name}', 'PagesController@thanks')->name('thanks');
 
+/*Route::post('/contact', function () {
+    $data = request() ->all();
+    echo "Name: " . $data['name']. '<br>';
+    echo "Email: " . $data['email']. '<br>';
+    echo "Message: " . $data['body'];
+});*/
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
