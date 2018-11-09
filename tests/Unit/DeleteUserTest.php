33596f5bfd7b01395d4e1c523c8f9ef18abce230
alexandraflores <af345@njit.edu>
2018-11-09 06:05:54 -0500

    7.  Create a unit test to delete a user in the database.

---
+++ tests/Unit/DeleteUserTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\Unit;
+use App\User;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Tests\TestCase;
+class DeleteUserTest extends TestCase
+{
+    use SoftDeletes;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $this->assertClassHasAttribute('name', User::class);
+    }
+}
