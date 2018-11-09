1198a756f2cdbea40e616f229f0e926735c38554
alexandraflores <af345@njit.edu>
2018-11-09 15:21:06 -0500

    6.  Create a unit test to update the name of a user in the database to Steve Smith. (Can't make it work)

---
+++ tests/Unit/UpdateUserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserCountTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUserCountTest()
+    {
+        $user= User::all();
+        $this->assertGreaterThanOrEqual(0, count($user));
+        $this->assertEquals(50, count($user));
+        $this->assertLessThanOrEqual(50, count($user));
+    }
+}
