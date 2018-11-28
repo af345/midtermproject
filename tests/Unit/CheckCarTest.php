b6e7ad19845493115ec9fbaa59fabc95d4581d9e
alexandraflores <af345@njit.edu>
2018-11-27 20:26:12 -0500

    added thankyou.blade.php back to views

--- tests/Unit/CheckCarTest.php
+++ tests/Unit/CheckCarTest.php
@@ -5,15 +5,15 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class CheckCarTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function CheckCarTest()
     {
         $this->assertTrue(true);
     }
-}
+
