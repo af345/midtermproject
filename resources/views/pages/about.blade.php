0a5850ff75e69d92e36d22b2c9960369ed278d77
alexandraflores <af345@njit.edu>
2018-12-03 21:17:21 -0500

    fixing DB error on heroku

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
 
         <h1><b>This is My About Page.</b></h1>
-        <p>Hello World <br>This is all about Laravel</p>
+        <p>Hello World! <br>This is all about Laravel</p>
 
     </div>
 
